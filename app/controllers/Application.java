03787560c69a69bb486a44025c96c428e98a0c4f
mykola bilokonsky <mykola@mykolas-air.gateway.2wire.net>
2013-12-27 14:30:58 -0500

    Web socket works, app now wrapped for intellij

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,9 +1,10 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import play.libs.F;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.mvc.WebSocket;
+import views.html.index;
 
 public class Application extends Controller {
 
@@ -11,4 +12,23 @@ public class Application extends Controller {
         return ok(index.render("Your new application is ready."));
     }
 
+    public static WebSocket<String> connectToSocket(String socket) {
+    	return new WebSocket<String>() {
+    		public void onReady(WebSocket.In<String> in, WebSocket.Out<String> out) {
+    			in.onMessage(new F.Callback<String>() {
+    				public void invoke(String event) {
+    					System.out.println("Input Received: " + event);
+    				}
+    			});
+
+    			in.onClose(new F.Callback0() {
+    				public void invoke() {
+    					System.out.println("Socket Connection Closed.");	
+    				}
+    			});
+
+    			out.write("Hello!");
+    		}
+    	};
+    }
 }
