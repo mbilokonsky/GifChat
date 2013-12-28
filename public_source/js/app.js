48dd43a8a6f2a96517014947f157a332c819b6df
mykola bilokonsky <mykola@mykolas-air.gateway.2wire.net>
2013-12-28 17:51:34 -0500

    ok, pulled out that napa crap so front-end src now has its own Bower configuration.

--- public_source/js/app.js
+++ public_source/js/app.js
@@ -1,17 +1,11 @@
 // This will include ./node_modules/angular/angular.js
 // and give us access to the `angular` global object.
-require('angular/angular');
-require('angular-route/angular-route');
+require('../bower_components/angular/angular.js');
+require("../bower_components/angular-bootstrap/ui-bootstrap-tpls.js");
+
 require("./services/services");
 require("./filters/filters");
 require("./directives/directives");
 
 // Create your app
-angular.module('GifChat', ['ngRoute', "GifChat.services", "GifChat.filters", "GifChat.directives"])
-    .config(['$routeProvider', function($routeProvider) {
-
-    // Specify routes to load our partials upon the given URLs
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+angular.module('GifChat', ["ui.bootstrap", "GifChat.services", "GifChat.filters", "GifChat.directives"]);
